92ff88a5585edc47700d461755252b7444ebcbc1
WenXin2 <dwnewville@gmail.com>
2025-08-16 19:46:09 -0500

    Improve gen

--- src/main/java/com/wenxin2/dt_sonicraft/block/BeanstalkBranchBlock.java
+++ src/main/java/com/wenxin2/dt_sonicraft/block/BeanstalkBranchBlock.java
@@ -1,12 +1,9 @@
 package com.wenxin2.dt_sonicraft.block;
 
 import com.ferreusveritas.dynamictreesplus.block.CactusBranchBlock;
-import javax.annotation.Nullable;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -17,12 +14,4 @@ public class BeanstalkBranchBlock extends CactusBranchBlock {
 
     @Override
     public void entityInside(BlockState state, Level worldIn, BlockPos pos, Entity entity) {}
-
-    @Nullable
-    @Override
-    public BlockState getStateForPlacement(BlockPlaceContext context) {
-        BlockState returnState = this.defaultBlockState();
-        boolean trunk = context.getClickedFace() == Direction.UP;
-        return returnState.setValue(TRUNK_TYPE, trunk ? CactusThickness.TRUNK : CactusThickness.BRANCH).setValue(ORIGIN, context.getClickedFace() != Direction.DOWN ? context.getClickedFace().getOpposite() : Direction.DOWN);
-    }
 }
